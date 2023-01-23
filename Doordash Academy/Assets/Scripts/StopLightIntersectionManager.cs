525908ee038fc1bd1edce220f2b32c9c06cda7e0
connorespig <cespig@cs.washington.edu>
2023-01-22 22:15:58 -0800

    Intersections

---
+++ Doordash Academy/Assets/Scripts/StopLightIntersectionManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class StopLightIntersectionManager : MonoBehaviour
+{
+    public Light2D north;
+    public Light2D east;
+    public Light2D south;
+    public Light2D west;
+
+    public void Start() {
+
+    }
+
+    public void Update() {
+        
+    }
+}
