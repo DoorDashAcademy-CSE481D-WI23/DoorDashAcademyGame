45b8f55252a9bdf31dbf410f819c1647b868a9af
connorespig <cespig@cs.washington.edu>
2023-02-01 18:07:30 -0800

    update to stop cars from stopping in front of wall

--- Doordash Academy/Assets/Scripts/VehicleBehavior.cs
+++ Doordash Academy/Assets/Scripts/VehicleBehavior.cs
@@ -24,14 +24,14 @@ public class VehicleBehavior : MonoBehaviour
     {
         velocity = InitialVelocity;
         hitbox = GetComponent<BoxCollider2D>();
-        layerMask = LayerMask.GetMask("Blocking", "Actor", "Ignore Collisions");
+        layerMask = LayerMask.GetMask("Actor", "Ignore Collisions");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if (Time.time > ignoreIntersectionColliders) {
-            layerMask = LayerMask.GetMask("Blocking", "Actor", "Ignore Collisions");
+            layerMask = LayerMask.GetMask("Actor", "Ignore Collisions");
         }
 
         RaycastHit2D hit;
@@ -59,7 +59,7 @@ public class VehicleBehavior : MonoBehaviour
                 transform.position = pos;
             } else {
                 if (hit.distance - lastDetectionRange < -1) {
-                    layerMask = LayerMask.GetMask("Blocking", "Actor");
+                    layerMask = LayerMask.GetMask("Actor");
                     ignoreIntersectionColliders = Time.time + 0.5f;  // Ignore intersection lights for one second.
                 }
                 ApplyBrake();
