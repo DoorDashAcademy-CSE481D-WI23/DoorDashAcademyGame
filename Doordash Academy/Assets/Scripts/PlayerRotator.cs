fdb9087c06cec4e5c07bb090a101915af23708bd
colehahn <colehahn2000@gmail.com>
2023-02-10 00:51:46 -0800

    added animated bike sprite to player!

--- Doordash Academy/Assets/Scripts/PlayerRotator.cs
+++ Doordash Academy/Assets/Scripts/PlayerRotator.cs
@@ -8,11 +8,13 @@ public class PlayerRotator : MonoBehaviour
 {
     // Sprites should represent the object turning uniformly from straight up
     // to straight down, to the right
-    public Sprite[] sprites;
+    public int numAngles = 5;
+    public Animator anim;
 
     private Transform parentTransform;
     private Transform t;
     private SpriteRenderer spriteRenderer;
+    private CarController carcontroller;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,7 @@ public class PlayerRotator : MonoBehaviour
         t = gameObject.GetComponent<Transform>();
         parentTransform = t.parent;
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        carcontroller = parentTransform.gameObject.GetComponent<CarController>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,7 @@ public class PlayerRotator : MonoBehaviour
     {
         float r = parentTransform.localEulerAngles.z % 360f;
         setSpriteGivenRotation(r);
+        anim.speed = carcontroller.GetVelocity().magnitude / carcontroller.maxSpeed;
     }
 
     private void setSpriteGivenRotation(float r)
@@ -36,8 +40,8 @@ public class PlayerRotator : MonoBehaviour
             shouldFlipY = true;
             r = 360f - r;
         }
-        int spriteNum = Mathf.RoundToInt((r - 360f) / -180f * (sprites.Length - 1));
-        spriteRenderer.sprite = sprites[spriteNum];
-        t.localEulerAngles = new Vector3(0f, shouldFlipY ? 180f : 0f, -360f + (1.0f * spriteNum / (sprites.Length - 1) * 180f));
+        int spriteNum = Mathf.RoundToInt((r - 360f) / -180f * (numAngles - 1));
+        anim.SetInteger("spriteNum", spriteNum);
+        t.localEulerAngles = new Vector3(0f, shouldFlipY ? 180f : 0f, -360f + (1.0f * spriteNum / (numAngles - 1) * 180f));
     }
 }
